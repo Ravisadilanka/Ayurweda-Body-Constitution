351c42daee9cee9b5753814437aa64e0897594d9
Chamaali <chamaalidilka@gmail.com>
2024-06-16 09:48:22 +0530

    Final result is shown.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+// import TestTwo from './componets/TestTwo';
 import Test from './componets/Test';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     {/* <TestTwo /> */}
      <Test />
+
     </main>
   );
 }
